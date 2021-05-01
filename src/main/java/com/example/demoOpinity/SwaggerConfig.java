f3dab599bef14864ab1ef707ada4a6f7d7deea36
TenseiRyuuga <stijntjedelion@hotmail.com>
2021-05-01 17:58:30 +0200

    description correction

--- src/main/java/com/example/demoOpinity/SwaggerConfig.java
+++ src/main/java/com/example/demoOpinity/SwaggerConfig.java
@@ -3,15 +3,12 @@ package com.example.demoOpinity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicate;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.regex;
-import com.google.common.base.Predicates;
 
 @Configuration
 @EnableSwagger2
@@ -23,17 +20,13 @@ public class SwaggerConfig {
 				.apiInfo(apiInfo()).select().paths(postPaths()).build();
 	}
 
-	private Predicate<String> postPaths() {
-		return regex("/api/posts.*").or(regex("/api/javainuse.*"));
-
+	private java.util.function.Predicate<String> postPaths() {
+		return regex("/demoOpinity");
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("JavaInUse API")
-				.description("JavaInUse API reference for developers")
-				.termsOfServiceUrl("http://javainuse.com").build();
-//				.contact("javainuse@gmail.com").license("JavaInUse License")
-//				.licenseUrl("javainuse@gmail.com").version("1.0").build();
+		return new ApiInfoBuilder().title("Opinity API")
+				.description("Opinity API made by Stijn de Leeuw").build();
 	}
 
 }
