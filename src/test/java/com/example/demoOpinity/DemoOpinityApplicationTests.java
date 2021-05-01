7319a421a5b0bdee13263de24e3dc71994e4c3bb
TenseiRyuuga <stijntjedelion@hotmail.com>
2021-05-01 17:44:28 +0200

    first commit

---
+++ src/test/java/com/example/demoOpinity/DemoOpinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoOpinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
