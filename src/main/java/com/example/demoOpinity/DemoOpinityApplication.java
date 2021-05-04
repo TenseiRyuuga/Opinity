212eedf1f3886076dc470cdee025c99906b17ab3
TenseiRyuuga <stijntjedelion@hotmail.com>
2021-05-04 16:02:56 +0200

    v2 swagger-ui working fileupload working and excel to POJO implemented in a flexible way

--- src/main/java/com/example/demoOpinity/DemoOpinityApplication.java
+++ src/main/java/com/example/demoOpinity/DemoOpinityApplication.java
@@ -1,11 +1,19 @@
 package com.example.demoOpinity;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demoOpinity.storage.StorageProperties;
+import com.example.demoOpinity.storage.StorageService;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//enable the configuration StorageProperties
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 @EnableSwagger2
 public class DemoOpinityApplication {
 
@@ -13,4 +21,11 @@ public class DemoOpinityApplication {
 		SpringApplication.run(DemoOpinityApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
